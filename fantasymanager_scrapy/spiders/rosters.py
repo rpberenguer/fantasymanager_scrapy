a274f7b36e049644412972b33cd2622513ab86de
raul <rpberenguer@gmail.com>
2020-02-27 23:12:32 +0100

    mejoras

--- fantasymanager_scrapy/spiders/rosters.py
+++ fantasymanager_scrapy/spiders/rosters.py
@@ -4,10 +4,13 @@ import scrapy
 
 class RostersSpider(scrapy.Spider):
     name = 'rosters'
-    allowed_domains = ['http://espn.go.com']
-    start_urls = ['http://http://espn.go.com/nba/teams/']
+    allowed_domains = ['espn.com']
+    start_urls = ['https://www.espn.com/nba/teams']
 
     def parse(self, response):
         teams = response.xpath("//a[starts-with(@href, '/nba/team/roster/_/name/')]/@href").extract()
         for team in teams:
-            print(team)
+            yield scrapy.Request('http://www.espn.com' + team, callback=self.parse_team)
+
+    def parse_team(self, response):
+        print("parsing team...")
