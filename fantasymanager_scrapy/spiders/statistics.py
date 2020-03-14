8feea138e93cf93c4a4382032a4f9b81e18c55ed
raul <rpberenguer@gmail.com>
2020-03-14 23:38:27 +0100

    mejoras statistics

--- fantasymanager_scrapy/spiders/statistics.py
+++ fantasymanager_scrapy/spiders/statistics.py
@@ -17,4 +17,15 @@ class StatisticsSpider(scrapy.Spider):
             gameLinks = tableDay.xpath(".//tbody/tr/td[1]/a[starts-with(@href, '/nba/team/_/name/')]/../../td[2]/div/a[starts-with(@href, '/nba/team/_/name/')]/../../../td[3]/a[starts-with(@href, '/nba/game?gameId=')]/@href").getall()
 
             for gameLink in gameLinks:
-                print("Game link : " + gameLink)
+                yield scrapy.Request('http://www.espn.com/nba/boxscore?gameId=' + gameLink[17:], callback=self.parse_game)
+
+    def parse_game(self, response):
+        print("parsing game..." + response.url)
+        teamHomeDiv = response.css("div.team.home")
+        teamAwayDiv = response.css("div.team.away")
+        yield{
+            'teamHome': teamHomeDiv.xpath(".//a[starts-with(@href, '/nba/team/_/name/')]/@href").get(),
+            'teamHomeScore': teamHomeDiv.css("div.score.icon-font-before::text").get(),
+            'teamAway': teamAwayDiv.xpath(".//a[starts-with(@href, '/nba/team/_/name/')]/@href").get(),
+            'teamAwayScore': teamAwayDiv.css("div.score.icon-font-after::text").get()
+        }
